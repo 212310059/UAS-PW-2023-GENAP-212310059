5881fdb6f7cb262f934efffe4ec585c74a34af03
212310059 <120255858+212310059@users.noreply.github.com>
2023-07-25 19:51:54 +0900

    finp

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <tittle>TA - Pemrog Web</title>
+    </head>
+
+<body>
+    <h1>Homepage With viewBox</h1>
+</body>
+</html>
